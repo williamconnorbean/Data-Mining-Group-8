a5e25dbe8ef6be9e4302e3da6021cad702773fd0
Claire Chen <ca.claire.chen@gmail.com>
2020-12-17 18:33:21 -0600

    mining for each month

---
+++ mining/time/timeMining.py
@@ -0,0 +1,36 @@
+from fp_growth import find_frequent_itemsets
+import csv
+import datetime
+
+NUM_MONTH = 12
+
+transactions = []
+for i in range(NUM_MONTH + 1):
+    transactions_monthly = []
+    transactions.append(transactions_monthly)
+
+with open("../data/canada-covid-details-reduced.csv") as database:
+    for row in csv.reader(database):
+        date_time_str = row[0]
+        date_time_obj = datetime.datetime.strptime(date_time_str, '%Y/%m/%d %H:%M:%S')
+        transactions[date_time_obj.month-1].append(row[2:4])
+        transactions[NUM_MONTH].append(row[2:4])
+
+minsup = 2
+for i in range(NUM_MONTH + 1):
+    print("\n\nMonth #{}:".format(i+1))
+    print("Total transactions: {}\n".format(len(transactions[i])))
+    print("-------------------------------")
+
+    maxCount = 0
+    frequentItem = None
+
+    for itemset, support in find_frequent_itemsets(transactions[i], minsup, True):
+        if len(itemset) == 2 and 'Not Reported' not in itemset:
+            if support > maxCount:
+                maxCount = support
+                frequentItem = itemset
+    maxSupport = 0
+    if len(transactions[i]) != 0:
+        maxSupport = (float(maxCount)/float(len(transactions[i]))) * 100
+    print("{0}, support {1:.2f}%".format(frequentItem, maxSupport))
