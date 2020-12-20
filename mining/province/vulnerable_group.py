ca47945a0f9287646fc6afbde3cacb401a6440df
jialiang he <eddy@jialiangs-MBP.hitronhub.home>
2020-12-19 21:08:03 -0600

    fix mining with old algorithm

--- mining/province/vulnerable_group.py
+++ mining/province/vulnerable_group.py
@@ -1,5 +1,7 @@
 import sys
 import os
+sys.path.append('../../')
+
 sys.path.insert(0, os.path.dirname(os.path.dirname(os.getcwd()))+'/FP-Growth')
 
 from fp_growth import find_frequent_itemsets
@@ -13,18 +15,74 @@ gender = ['Male', 'Female']
 transactions = []
 with open("../../data/canada-covid-details-reduced.csv") as database:
     for row in csv.reader(database):
-        transactions.append(row)
+        tran = row[2:4]
+        tran.append(row[6])
+        # print tran
+        transactions.append(tran)
 
-minsup = 10
-for itemset in find_frequent_itemsets(transactions, minsup, True):
-    if(len(itemset[0]) == 3 and 
-        (
-            (list(itemset[0])[0] in gender and list(itemset[0])[1] in age_gourps and list(itemset[0])[2] in prov) or
-            (list(itemset[0])[0] in gender and list(itemset[0])[1] in prov and list(itemset[0])[2] in age_gourps) or
-            (list(itemset[0])[0] in age_gourps and list(itemset[0])[1] in prov and list(itemset[0])[2] in gender) or 
-            (list(itemset[0])[0] in age_gourps and list(itemset[0])[1] in gender and list(itemset[0])[2] in prov) or 
-            (list(itemset[0])[0] in prov and list(itemset[0])[1] in age_gourps and list(itemset[0])[2] in gender) or
-            (list(itemset[0])[0] in prov and list(itemset[0])[1] in gender and list(itemset[0])[2] in age_gourps)
-        )):
-        print itemset
+results = []
+if str(sys.argv[1]) == "ALL":
+    minsup = 10
+    for itemset, count in find_frequent_itemsets(transactions, minsup, include_support=True):
+        # print itemset, count
+        if(len(itemset) == 2 and (itemset[0] in age_gourps and itemset[1] in prov)):
+            results.append([itemset[1], itemset[0], count])
+        elif ( len(itemset) == 2 and  (itemset[0] in prov and itemset[1] in age_gourps)):
+            itemset.append(count)
+            results.append(itemset)
+    for item in prov:
+        temp = []
+        for result in results:
+            if(result[0] == item):
+                temp.insert(0, result)
+            else:
+                temp.append(result)
+        results = temp
+    
+    temp = ''
+    for result in results:
+        if(result[0] != temp):
+            print ["Prov", "Age Group", "Count"]
+        temp = result[0]
+        print result
 
+else:
+    minsup = 10
+    for itemset, count in find_frequent_itemsets(transactions, minsup, include_support=True):
+        if(len(itemset) == 3 and (itemset[0] in gender and itemset[1] in age_gourps and itemset[2] in prov) ):
+            results.append([itemset[2], itemset[0], itemset[1], count])
+        elif(len(itemset) == 3 and (itemset[0] in gender and itemset[1] in prov and itemset[2] in age_gourps) ):
+            results.append([itemset[1], itemset[0], itemset[2], count])
+        elif(len(itemset) == 3 and (itemset[0] in age_gourps and itemset[1] in prov and itemset[2] in gender) ):
+            results.append([itemset[1], itemset[2], itemset[0], count])
+        elif(len(itemset) == 3 and (itemset[0] in age_gourps and itemset[1] in gender and itemset[2] in prov) ):
+            results.append([itemset[2], itemset[1], itemset[0], count])
+        elif(len(itemset) == 3 and (itemset[0] in prov and itemset[1] in age_gourps and itemset[2] in gender) ):
+            results.append([itemset[0], itemset[2], itemset[1], count])
+        elif(len(itemset) == 3 and (itemset[0] in prov and itemset[1] in gender and itemset[2] in age_gourps) ):
+            results.append([itemset[0], itemset[1], itemset[2], count])
+    
+    for item in prov:
+        temp = []
+        for result in results:
+            if(result[0] == item):
+                temp.insert(0, result)
+            else:
+                temp.append(result)
+        results = temp
+    
+    for item in gender:
+        temp = []
+        for result in results:
+            if(result[1] == item):
+                temp.insert(0, result)
+            else:
+                temp.append(result)
+        results = temp
+        
+    temp = ''
+    for result in results:
+        if(result[0] != temp):
+            print ["Prov", "Gender", "Age Group", "Count"]
+        temp = result[0]
+        print result
