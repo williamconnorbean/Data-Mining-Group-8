af5c858644d75d1a7b03035048161a622ce02623
Claire Chen <ca.claire.chen@gmail.com>
2020-12-19 20:00:51 -0600

    update using new FP alg

--- mining/newFP/compare.py
+++ mining/newFP/compare.py
@@ -9,41 +9,47 @@ import csv
 import time
 
 minsup = 3
+for filename in os.listdir("../../data/differentDB"):
+    if filename.endswith(".csv"): 
+        print filename
+        transactions = []
+        start1 = time.time()
 
-transactions = []
-start1 = time.time()
+        # first tdb scan
+        with open("../../data/differentDB/"+filename) as database:
+            for row in csv.reader(database):
+                transactions.append(row)
+        print "Num of transaction: %d " %  len(transactions)
+        print "Num of items: %d " %  len(transactions[0])
+        end1 = time.time()
+        print "\n\nFP Growth"
+        print "Time for 1st db scan: %f " %  (end1 - start1)
 
-# first tdb scan
-with open("../../data/canada-covid-details-reduced.csv") as database:
-    for row in csv.reader(database):
-        transactions.append(row)
-print "Num of transaction: %d " %  len(transactions)
-print "Num of items: %d " %  len(transactions[0])
-end1 = time.time()
-print "FP Growth"
-print "Time for 1st db scan: %f " %  (end1 - start1)
-start2 = time.time()
-# mine frequent itemsets using original FP-Growth with second scan
-find_frequent_itemsets(transactions, minsup, "../../data/canada-covid-details-reduced.csv", True)
-end2 = time.time()
+        start2 = time.time()
+        # mine frequent itemsets using original FP-Growth with second scan
+        for itemset, support in find_frequent_itemsets(transactions, minsup, "../../data/differentDB/"+filename, True, True):
+            pass
 
-print "Time for mining with 2nd DB scan : %f " %  (end2 - start2)
-print "Total time : %f " %  (end2 - start1)
+        end2 = time.time()
 
-dbTree = DBTree.DBTree()
-start1 = time.time()
-# first tdb scan
-with open("../../data/canada-covid-details-reduced.csv") as database:
-    for row in csv.reader(database):
-        dbTree.add(row)
-end1 = time.time()
-print "\n\nNew FP Growth"
-print "Time for 1st db scan: %f " %  (end1 - start1)
-start2 = time.time()
+        print "Time for mining with 2nd DB scan : %f " %  (end2 - start2)
+        print "Total time : %f " %  (end2 - start1)
 
-# mine frequent itemsets using New FP-Growth with DBTree with second scan
-find_frequent_itemsets_1_tdb_scan(dbTree, minsup)
-end2 = time.time()
-print "Time for mining without 2nd DB scan : %f " %  (end2 - start2)
-print "Total time : %f " %  (end2 - start1)
+        dbTree = DBTree.DBTree()
+        start1 = time.time()
+        # first tdb scan
+        with open("../../data/canada-covid-details-reduced.csv") as database:
+            for row in csv.reader(database):
+                dbTree.add(row)
+        end1 = time.time()
+        print "\n\nNew FP Growth"
+        print "Time for 1st db scan: %f " %  (end1 - start1)
+        start2 = time.time()
 
+        # mine frequent itemsets using New FP-Growth with DBTree with second scan
+        for itemset, support in find_frequent_itemsets_1_tdb_scan(dbTree, minsup, True):
+            pass
+
+        end2 = time.time()
+        print "Time for mining without 2nd DB scan : %f " %  (end2 - start2)
+        print "Total time : %f " %  (end2 - start1)
